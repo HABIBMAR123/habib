4b463befa2221faa83c77b2d1fd24d84d15e7d99
Habib muzaki <captberrel@gmail.com>
2026-04-25 01:19:07 +0700

    Create LoopKotak.java

---
+++ LoopKotak.java
@@ -0,0 +1,11 @@
+public class LoopKotak {
+    public static void main(String[] args) {
+        int sisi = 5;  
+        for (int i = 1; i <= sisi; i++) {       
+            for (int j = 1; j <= sisi; j++) {    
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
